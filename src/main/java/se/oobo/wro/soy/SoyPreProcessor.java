ec44857f524728f5a189a7c97b59658773509f83
jaerss <jan.eriksson@metria.se>
2012-08-28 15:59:13 +0200

    added SayJsSrcOptions config

--- src/main/java/se/oobo/wro/soy/SoyPreProcessor.java
+++ src/main/java/se/oobo/wro/soy/SoyPreProcessor.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
 
 import ro.isdc.wro.model.resource.Resource;
 import ro.isdc.wro.model.resource.processor.ResourcePreProcessor;
@@ -14,6 +15,7 @@ import ro.isdc.wro.model.resource.processor.ResourcePreProcessor;
 import com.google.common.io.InputSupplier;
 import com.google.template.soy.SoyFileSet;
 import com.google.template.soy.jssrc.SoyJsSrcOptions;
+import com.google.template.soy.jssrc.SoyJsSrcOptions.CodeStyle;
 
 public class SoyPreProcessor implements ResourcePreProcessor {
 
@@ -22,6 +24,8 @@ public class SoyPreProcessor implements ResourcePreProcessor {
 	public SoyPreProcessor() {
 		try {
 			configuration = new PropertiesConfiguration("wro-soy.properties");
+			configuration
+					.setReloadingStrategy(new FileChangedReloadingStrategy());
 		} catch (ConfigurationException e) {
 			throw new RuntimeException(e);
 		}
@@ -39,13 +43,39 @@ public class SoyPreProcessor implements ResourcePreProcessor {
 				}, resource.getUri()).build();
 
 		try {
-			List<String> jsSources = sfs.compileToJsSrc(new SoyJsSrcOptions(),
+			List<String> jsSources = sfs.compileToJsSrc(crateSoyJsOptions(),
 					null);
 			writer.write(jsSources.get(0));
 			writer.close();
 		} catch (Exception e) {
 			writer.write("Failed to render soy file: " + resource.getUri()
 					+ "\n" + e.toString() + "\n\n");
+		}
+	}
+
+	private SoyJsSrcOptions crateSoyJsOptions() {
+		SoyJsSrcOptions opts = new SoyJsSrcOptions();
+		opts.setBidiGlobalDir(configuration.getInt("wro4j.soy.bidiGlobalDir"));
+		opts.setCodeStyle(parseCodeStyle(configuration.getString("wro4j.soy.codestyle")));
+		opts.setGoogMsgsAreExternal(configuration.getBoolean("wro4j.soy.googMsgsAreExternal"));
+		opts.setShouldAllowDeprecatedSyntax(configuration.getBoolean("wro4j.soy.shouldAllowDeprecatedSyntax"));
+		opts.setShouldDeclareTopLevelNamespaces(configuration.getBoolean("wro4j.soy.shouldDeclareTopLevelNamespaces"));
+		opts.setShouldGenerateGoogMsgDefs(configuration.getBoolean("wro4j.soy.shouldGenerateGoogMsgDefs"));
+		opts.setShouldGenerateJsdoc(configuration.getBoolean("wro4j.soy.shouldGenerateJsdoc"));
+		opts.setShouldProvideRequireJsFunctions(configuration.getBoolean("wro4j.soy.shouldProvideRequireJsFunctions"));
+		opts.setShouldProvideRequireSoyNamespaces(configuration
+				.getBoolean("wro4j.soy.shouldProvideRequireSoyNamespaces"));
+		return opts;
+	}
+
+	private CodeStyle parseCodeStyle(String style) {
+		if ("concat".equalsIgnoreCase(style)) {
+			return CodeStyle.CONCAT;
+		} else if ("stringbuilder".equalsIgnoreCase(style)) {
+			return CodeStyle.STRINGBUILDER;
+		} else {
+			throw new RuntimeException(
+					"Unhandled parameter value for codestyle: " + style);
 		}
 	}
 
